37fa9a11aa66cf2d45ad952ade4d86794be1aba6
Einss1 <luan_navarro@hotmail.com>
2021-05-04 14:04:52 +0100

    Commit 5.1 - 04/05/2021
    Creation of the food calculator function in the staff area.

--- JavaScript/js_staff.js
+++ JavaScript/js_staff.js
@@ -1,3 +1,5 @@
+/* Customer Generator */
+
 const url = ' https://randomuser.me/api/?nat=ie ';
 
 let avatar = document.getElementById('avatar');
@@ -39,3 +41,38 @@ function updateProfile(profile) {
 function printError(error) {
     console.log(error);
 }
+
+/* Food Cost */
+
+function calculate() {
+
+price1 = document.getElementById("nachos").value*5;
+price2 = document.getElementById("tacos").value*7;
+price3 = document.getElementById("churros").value*5;
+price4 = document.getElementById("steak").value*18;
+price5 = document.getElementById("salad").value*13;
+price6 = document.getElementById("ribeye").value*22;
+price7 = document.getElementById("coke").value*3;
+price8 = document.getElementById("milkshake").value*5;
+price9 = document.getElementById("water").value*1.50;
+price10 = document.getElementById("gateau").value*7;
+price11 = document.getElementById("cake").value*3;
+price12 = document.getElementById("cream").value*3;
+
+document.getElementById("totalbill").innerHTML = price1 + price2 + price3 + price4
++ price5 + price6 + price7 + price8 + price9 + price10 + price11 + price12 + ' €';
+
+document.getElementById("starters").innerHTML = price1 + price2 + price3 + ' €';
+
+document.getElementById("main").innerHTML = price4 + price5 + price6 + ' €';
+
+document.getElementById("dessert").innerHTML = price10 + price11 + price12 + ' €';
+
+document.getElementById("drinks").innerHTML = price7 + price8 + price9 + ' €';
+
+document.getElementById("vegetarian").innerHTML = price1 + price3 + price5 + ' €';
+
+document.getElementById("nonvegetarian").innerHTML = price2 + price4 + price6 + ' €';
+}
+
+
